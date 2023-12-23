a98c99234d1b90b7b9616040b98891627d37bf41
wangshaohui <97082645@qq.com>
2023-12-23 20:06:13 +0800

    commit empty files

---
+++ transform-ts/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+#include "transform-systime.h"
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
---
+++ transform-ts/transform-systime.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <mutex>
+
+class transform_systime
+{
+public:
+	transform_systime() = default;
+	virtual ~transform_systime() = default;
+
+	void reset() {}
+
+	void transform_to_system() {}
+
+private:
+
+};
